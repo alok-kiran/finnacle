a412cdde35faaa729c4744ce7dc24b5147887420
alok-kiran <alokkiran777@gmail.com>
2025-02-19 02:10:07 +0400

    Add utility functions and UI components for dashboard and form elements

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,43 @@
+import { SignIn, ClerkLoaded, ClerkLoading } from '@clerk/nextjs'
+import { Loader2 } from 'lucide-react'
+import Image from 'next/image'
+
+export default function Page() {
+  return (
+    <div className=' min-h-screen grid grid-cols-1 place-items-center lg:grid-cols-2'>
+      <div className=' h-full lg:flex flex-col items-center justify-center px-4'>
+      <div className=' text-center space-y-4 pt-16'>
+        <h1 className='font-bold text-3xl text-[#2E2A47]'>
+          Welcome back!
+        </h1>
+        <p className=' text-base text-[#7E8CA0]'>
+          Log in or sign up to continue
+        </p>
+
+      <div className=' flex items-center justify-center mt-8'>
+        <ClerkLoaded>
+          <SignIn
+            path='/sign-in'
+            signUpForceRedirectUrl="/sign-up"
+            appearance={{
+              elements: {
+                avatarBox: {
+                  height: 30,
+                  width: 30,
+                }
+              }
+            }}
+          />
+        </ClerkLoaded>
+        <ClerkLoading>
+          <Loader2 size={32} className=' animate-spin text-muted-foreground' />
+          </ClerkLoading>
+      </div>
+      </div>
+      </div>
+      <div className=' h-full w-full bg-blue-600 hidden lg:flex items-center justify-center'>
+        <Image src='/logo.svg' width={200} height={200} alt='logo' />
+      </div>
+    </div>
+  )
+}
